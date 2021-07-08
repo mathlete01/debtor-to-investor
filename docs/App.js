658951e05af16ce37134d6514bdc9e53960f8563
mathlete01 <msallin@gmail.com>
2021-07-08 16:59:58 -0700

    Add app.js and index.js to docs

---
+++ docs/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Debtor to Investor</header>
+    </div>
+  );
+}
+
+export default App;
